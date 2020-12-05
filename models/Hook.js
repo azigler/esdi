dc921ed29a89bcdaac9e9334c602ac85f1bc43c5
Andrew Zigler <andrewzigler@gmail.com>
2020-12-05 10:34:43 -0800

    Refactor Hook for channels

--- models/Hook.js
+++ models/Hook.js
@@ -12,16 +12,18 @@ class Hook {
    * @param {String} config.type Hook type
    * @param {String} [config.description = ''] Hook description
    * @param {Function} config.init function that initializes the Hook on the Esdi server
-   * @param {Function} [config.toggle = () => {}] function that toggles the Hook for a channel
+   * @param {Function} [config.enable = () => {}] function that enables the Hook for a discord.js Channel
+   * @param {Function} [config.disable = () => {}] function that runs before disabling the Hook for a discord.js Channel
    * @param {String} sourcePath full path of Hook source file
    * @constructor
    */
-  constructor ({ name, type = 'global', description = '', init, toggle = () => {} }, sourcePath) {
+  constructor ({ name, type = 'global', description = '', init, enable = () => {}, disable = () => {} }, sourcePath) {
     this.name = name
     this.type = type
     this.description = description
     this.init = init
-    this.toggle = toggle
+    this.enable = enable
+    this.disable = disable
     this.sourcePath = sourcePath
   }
 }
