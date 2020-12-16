ac0f18b403f02177a7e5be45e0bed69c0403b8ae
Andrew Zigler <andrewzigler@gmail.com>
2020-12-16 15:02:45 -0800

    Add prefix command

--- commands/prefix.js
+++ commands/prefix.js
@@ -1,5 +1,5 @@
 /**
- * Shows or sets the custom {@link Command} prefix for this {@link Guild}
+ * Shows, sets, or resets the custom {@link Command} prefix for this {@link Guild}
  *
  * @type {Command}
  * @memberof Command
@@ -13,60 +13,61 @@
 module.exports = {
   name: 'prefix',
   ownerOnly: true,
-  usage: '[new prefix]',
-  description: 'Shows or sets the custom Command prefix for this server.',
+  usage: '[new prefix]/[reset]',
+  description: 'Shows, sets, or resets the custom Command prefix for this server.',
   async execute ({ message, args, server }) {
-    let guild
-    const msg = []
-
-    server.controllers.get('GuildController').forEach(g => {
-      if (g.id === message.guild.id) {
-        guild = g
-      }
-    })
-
-    const doc = await server.controllers.get('DatabaseController').fetchDoc({
+    // check this Guild's database document for a prefix
+    const { prefix } = await server.controllers.get('DatabaseController').fetchDoc({
       db: 'guild',
-      id: guild.id
+      id: message.guild.id
     })
 
+    const ESDI_PREFIX_REMINDER = 'Don\'t forget: you can always use the `esdi!` prefix!'
+
+    // if no arguments provided, announce the current prefix
     if (!args.length) {
-      const guildPrefix = doc.prefix
-
-      if (!guildPrefix) {
-        msg.push(`My prefix is \`${server.controllers.get('CommandController').determinePrefix(message)}\``)
+      if (!prefix) {
+        return message.channel.send(`\`${server.controllers.get('CommandController').determinePrefix(message)}\` is my command prefix.`)
       } else {
-        msg.push(`My prefix here is \`${doc.prefix}\``)
+        return message.channel.send(`\`${prefix}\` is my command prefix for this server. ${ESDI_PREFIX_REMINDER}`)
       }
+    // otherwise, handle setting or resetting the Guild's prefix
     } else {
-      const guildPrefix = args[0]
-
-      if (guildPrefix === 'reset') {
-        msg.push(`My prefix was reset back to \`${server.controllers.get('CommandController').determinePrefix(message)}\``)
+      // if argument is 'reset' or 'esdi!', reset the Guild's prefix
+      if (args[0] === 'reset' || args[0] === 'esdi!') {
         delete server.controllers.get('GuildController').get(message.guild.id).prefix
-        // TODO: only announce below if 'esdi!' is not prefix above
-        message.channel.send([msg.join(' '), '\nDon\'t forget: you can always use the `esdi!` prefix!'])
+
+        // determine reset prefix (either the instance prefix or the primary prefix)
+        const prefixObj = {
+          content: server.controllers.get('CommandController').prefix
+        }
+        const pre = server.controllers.get('CommandController').determinePrefix(prefixObj)
+
+        message.channel.send(`My command prefix was reset back to \`${pre}\` for this server.`)
+
         return server.controllers.get('DatabaseController').updateDoc({
           db: 'guild',
-          id: guild.id,
+          id: message.guild.id,
           payload: {
             prefix: undefined
           }
         })
       }
 
-      server.controllers.get('GuildController').get(guild.id).prefix = guildPrefix
-      msg.push(`My prefix here is now \`${guildPrefix}\``)
+      // determine the new prefix to set
+      const guildPrefix = args[0]
+
+      // set the new prefix
+      server.controllers.get('GuildController').get(message.guild.id).prefix = guildPrefix
+      message.channel.send(`\`${guildPrefix}\` is now my command prefix for this server. ${ESDI_PREFIX_REMINDER}`)
 
       server.controllers.get('DatabaseController').updateDoc({
         db: 'guild',
-        id: guild.id,
+        id: message.guild.id,
         payload: {
           prefix: guildPrefix
         }
       })
     }
-
-    message.channel.send([msg.join(' '), '\nDon\'t forget: you can always use the `esdi!` prefix!'])
   }
 }
